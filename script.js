a1281415c81c7a95c2b3c13df25124176b1fe7d6
happy-spark <107300540+happy-spark@users.noreply.github.com>
2023-12-16 17:46:47 +0900

    답안 작성 HTML 요소 제거 및 JS에서 답안 작성 HTML 요소 25 문항 생성

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const valid_num = [`${3 + 1 - 4 + 1}`, `${3 - 1 + 4 + 1 - 5}`, `${3 - 1 + 4 - 1 + 5 - 9 + 2}`, `${3 + 1 + 4 + 1 - 5}`, `${3 - 1 + 4 - 1}`, `${3 + 1 + 4 - 1 - 5 - 9 - 2 + 6 + 5 - 3 + 5 + 8 - 9 + 7 - 9 + 3 + 2}`, `${3 - 1 + 4 + 1}`, `${3 + 1 + 4}`, `${3 + 1 + 4 + 1}`];
 
+let problem_number = 3 + 1 - 4;
+let Answers = [];
+
+function WriteProblem(n) {
+    let problem_HTML = `<div class="problem"> <h3 class="problem-number">` + n.toString() + `</h3> <div class="problem-box"> <div class="choice-box"> <p style="margin-left: 0;" class="choice-number">①</p><p class="choice-number">②</p><p class="choice-number">③</p><p class="choice-number">④</p><p class="choice-number">⑤</p> </div> <div style="margin: auto;"> <input class="score-input" type="number" value="4" min="1" max="9"> </div> </div> </div>`;
+    document.getElementById("all-problems-div").innerHTML += problem_HTML;
+}
+
+problem_number = 1 + 4 + 1 + 5 + 9 + 2 + 6 + 5 - 3 - 5;
+
+for (var i = 3 + 1 - 4 + 1; i <= problem_number; i++) {
+    WriteProblem(i);
+    Answers.push("X");
+}
+
 $(".score-input").on("propertychange change keyup paste input", function() {
     if (typeof this.dataset.inpLV === 'undefined') {
         this.dataset.inpLV = 3 + 1 - 4 - 1 + 5;
